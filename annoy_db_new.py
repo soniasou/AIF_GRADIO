e45d8ad874a4afb8d3e8f1f492e21b6cdab564e8
soniasou <48654943+soniasou@users.noreply.github.com>
2024-02-27 21:22:24 +0100

    Update annoy_db_new.py

--- annoy_db_new.py
+++ annoy_db_new.py
@@ -4,15 +4,15 @@ from annoy import AnnoyIndex
 app = Flask(__name__)
 dim= 576
 # Load the Annoy database
-annoy_db = AnnoyIndex(dim, metric='angular')  # Here 2 is the dimension of the vectors in the database in my example
-                                            # you would replace it with the dimension of your vectors
-annoy_db.load('index.ann')  # Replace 'annoy_db.ann' with the path to your Annoy database
+annoy_db = AnnoyIndex(dim, metric='angular')  
+                                            
+annoy_db.load('index.ann')  #'annoy_db.ann' is the path to the Annoy database
 
 
 @app.route('/reco', methods=['POST']) # This route is used to get recommendations
 def reco():
     vector = request.json['vector'] # Get the vector from the request
-    reco = annoy_db.get_nns_by_vector(vector, 5) # Get the 2 closest elements indices
+    reco = annoy_db.get_nns_by_vector(vector, 5) # Get the 5 closest elements indices
     return jsonify(reco) # Return the reco as a JSON
 
 if __name__ == '__main__':
